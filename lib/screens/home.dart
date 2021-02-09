781c6e7fc1bd48d1a6508c59af4da71241168677
nadaabdu44 <nadaabdu333@gmail.com>
2021-02-09 22:29:49 +0300

    colors.red

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 import '../menu/MyDawerha.dart';
-class Home extends StatefulWidget{
-  State<StatefulWidget> createState(){
+
+class Home extends StatefulWidget {
+  State<StatefulWidget> createState() {
     return HomeState();
   }
 }
-class HomeState extends State<Home>{
+
+class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
         appBar: AppBar(
           title: Text("DAWERHA"),
-          backgroundColor: Colors.green,
+          backgroundColor: Colors.red,
           centerTitle: true,
           actions: <Widget>[
             IconButton(icon: Icon(Icons.control_point), onPressed: () {})
           ],
         ),
-        drawer: MyDrawer() ,//End Drawer
+        drawer: MyDrawer(), //End Drawer
         body: Column(),
       ),
     );
